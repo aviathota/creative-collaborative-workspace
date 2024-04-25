1e295863fb8c04b077c1493b3a0b301eb90b593b
aviathota <aviathota@gmail.com>
2024-04-25 14:35:06 -0400

    obfuscated firebase config

--- auth.py
+++ auth.py
@@ -1,16 +1,8 @@
 import pyrebase
 import app
+import db_secrets as sec
 
-config = {
-    'apiKey': "AIzaSyCWaWRquF1FDSetC25OYvt3WrT4dXXfO7s",
-    'authDomain': "ccw-auth.firebaseapp.com",
-    'projectId': "ccw-auth",
-    'storageBucket': "ccw-auth.appspot.com",
-    'messagingSenderId': "828962097424",
-    'appId': "1:828962097424:web:f2c19b0e334318bcd0fd95",
-    'measurementId': "G-6WLZ3G3S5J",
-    'databaseURL': ""
-}
+config = sec.config
 
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
